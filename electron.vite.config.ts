b4650486bb77a4faa06a2bbb8071dbe9b5ea181d
e-hua <eclipsehua@gmail.com>
2025-12-22 12:50:03 +0800

    chore: transplant electron-vite codebase into git repo

---
+++ electron.vite.config.ts
@@ -0,0 +1,23 @@
+import { resolve } from 'path'
+import { defineConfig } from 'electron-vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  main: {
+    resolve: {
+      alias: {
+        '@': resolve('src/main/src')
+      }
+    },
+  },
+  preload: {},
+  renderer: {
+    resolve: {
+      alias: {
+        '@': resolve('src/renderer/src'),
+      }
+    },
+    plugins: [react(), tailwindcss()]
+  }
+})
---
+++ src/main/src/server.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import type { Request, Response } from "express";
+import cors from "cors";
+import s3Router from "@/controllers/s3Controller";
+import ec2Router from "@/controllers/ec2Controller.js";
+import billRouter from "@/controllers/billController.js";
+import serviceRouter from "@/controllers/services/index.js";
+import testSSERouter from "@/controllers/testSSEController.js";
+
+const app = express();
+
+app.get("/health", (_: Request, res: Response) => {
+  res.send("Hello, CloudWrap!");
+});
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use("/s3", s3Router);
+app.use("/ec2", ec2Router);
+app.use("/bill", billRouter);
+app.use("/services", serviceRouter);
+app.use("/testSSE", testSSERouter);
+
+export default app;
